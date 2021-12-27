4b3fb4f5580cde918297c20a2649e4d018d8bf84
Dmitry <dima.fenix@list.ru>
2021-12-27 19:32:47 +0300

    Add validation for music

--- src/Middleware/musicSchema.js
+++ src/Middleware/musicSchema.js
@@ -1,1 +1,23 @@
 const Joi = require("joi");
+
+const musicSchema = {
+    addMusic: Joi.object({
+        name : Joi.string().min(1).max(50).required(),
+        authorId : Joi.required(),
+        genreId: Joi.string(),
+        file: Joi.string(),
+        avatar: Joi.string(),
+        text: Joi.string()
+    }),
+
+    changeMusic: Joi.object({
+        name : Joi.string().min(1).max(20).required(),
+        authorId : Joi.required(),
+        genreId: Joi.string(),
+        file: Joi.string(),
+        avatar: Joi.string(),
+        text: Joi.string()
+    })
+}
+
+module.exports = musicSchema;
--- src/Routers/musicRouter.js
+++ src/Routers/musicRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const controller = require('../Controllers/musicController')
 const router = express.Router();
+const validate = require("../Middleware/validation_schema");
+const musicSchema = require("../Middleware/musicSchema");
 
-router.post('/add', controller.addMusic); 
-router.put('/:id',controller.changeMusic);
+router.post('/add/:id', validate(musicSchema.addMusic), controller.addMusic); 
+router.put('/:id',  validate(musicSchema.changeMusic)  ,controller.changeMusic);
 router.get('/:id', controller.getById);
 router.delete('/:id', controller.deleteMusic);
 router.get('/all', controller.returnAllMusic);
